5f4a840410417d01f0a456c61a6e0aa64d43cff7
KanishkaV <kanishkav01@gmail.com>
2023-10-16 06:34:50 +0530

    minor fix in build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 pip install -r requirements.txt
-pyinstaller --onefile --add-data="gesture_model.h5:." --add-data="config/:config/" --hidden-import "mediapipe"  --name GesturePal.app App.py
+pyinstaller --onefile --add-data="gesture_model.h5:." --add-data="config/:config/" --hidden-import "mediapipe"  --name GesturePal App.py
